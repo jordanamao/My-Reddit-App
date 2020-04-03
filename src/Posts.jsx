8744a1f08a743f5774e844f4448ff9799377ae88
Runjia Mao <runjiamao@Runjias-MacBook-Pro.local>
2020-04-03 01:34:02 -0700

    history

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from "axios";
 
 let history = [{}];
-let historyStep = 1;
+let historyStep = 0;
 let firstUndo = false;
 
 class Posts extends Component {
@@ -57,7 +57,7 @@ class Posts extends Component {
   }
 
   addhandleClick = (f) => {
-    const fav = this.state.values.favorites
+    const fav = [...this.state.values.favorites];
     fav.push(f);
     
 
@@ -102,20 +102,22 @@ class Posts extends Component {
 
       if(historyStep === 0) {
         return;
-      } else if(firstUndo === false) {
-        console.log("History undo before shift: " + history);
-        //history = history.shift();
-        history = history.slice(1);
-        console.log("History undo after shift: " + history);
-        firstUndo = true;
       }
+      // } else if(firstUndo === false) {
+      //   console.log("History undo before shift: " + history);
+      //   //history = history.shift();
+      //   history = history.slice(1);
+      //   console.log("History undo after shift: " + history);
+      //   firstUndo = true;
+      // }
 
       historyStep -= 1;
       //history.pop();
       //history.pop();
-      var previous = [...history[historyStep]];
+      var previous = [...this.state.values.favorites];
+      var previous = [...history.slice(0, historyStep)];
       console.log(historyStep);
-      console.log("history 0 is " + history[historyStep]);
+      console.log("history is " + history[historyStep]);
       console.log(previous);
       // var favorite = [...this.state.values.favorites];
       // favorite.pop();
@@ -186,4 +188,176 @@ class Posts extends Component {
 }
 
 export default Posts;
+
+
+
+// import React, { Component } from 'react';
+// import axios from "axios";
+
+// // let history = [{}];
+//  let historyStep = 0;
+// // let firstUndo = false;
+
+// class Posts extends Component {
+//   state = {
+//     values: {
+//       posts: [],
+//       favorites: [],
+//       prevFav: [],
+//       nextFav: []
+//     }
+//   };
+
+//   //the right place to get data and call the server
+//   //is componentDidMount
+//   async componentDidMount(){
+
+//     //since it is nested object, use object destructing to desctruct them
+//     //used object destructuring to extract data by keys and renamed data to d and destructed two more times
+//     const {data:d} = await axios.get("https://www.reddit.com/.json")
+//     const {data:subdata} = d
+//     const {children:post} = subdata
+
+//     //console.log(posts);
+//     this.setState({ 
+//       values : {...this.state.values, posts: post}
+//     })
+//     //this.setState({posts});
+//   }
+
+
+//   handleChange = async (e) =>{
+
+//       let s;
+//       switch(e.target.value){
+        
+//         case "desc":
+//           s = this.state.values.posts.sort((a,b) => parseFloat(b.data.ups) - parseFloat(a.data.ups));
+//           this.setState({s})
+//           break;
+
+//         case "asce":
+//           s = this.state.values.posts.sort((a,b) => parseFloat(a.data.ups) - parseFloat(b.data.ups));
+//           this.setState({s})
+//           break;
+            
+//         case "hot":
+//           const {data:d} = await axios.get("https://www.reddit.com/.json")
+//           const {data:subdata} = d
+//           const {children:post} = subdata
+//           this.setState({values:{...this.state.values,posts:post}})
+//           break;
+//       }
+      
+//   }
+
+//   addhandleClick = (f) => {
+//     const prev = [...this.state.values.favorites];
+
+//     const fav = [...this.state.values.favorites];
+//     fav.push(f);
+
+//     historyStep += 1;
+//     console.log("Prev is :" + prev);
+//     console.log("Current is :" + fav);
+//     this.setState({values:{...this.state.values, favorites : fav, prevFav : prev}});
+ 
+
+
+
+//   }
+
+//   removehandleClick =  (f) => {
+//     const future = [...this.state.values.favorites];
+
+//     // history = history.slice(0, historyStep + 1);
+    
+//     const fav = [...this.state.values.favorites];
+//     fav.pop();
+    
+//     // history = history.concat([fav]);
+//     historyStep -= 1;
+
+//     this.setState({values:{...this.state.values,favorites:fav, nextFav : future}})
+
+//   }
+
+
+//   handleUndo = () => {
+
+//       if(historyStep === 0) {
+//         return;
+//       }
+
+//       historyStep -= 1;
+      
+//       // var prev = [...this.state.values.favorites];
+//       // var favorite = [...this.state.values.prevFav];
+//       console.log("previous : " + favorite);
+//       this.setState({
+//         values:{...this.state.values, favorites : previous}
+//         //favorites: [this.state.values.favorites, ...previous]
+//         //values:{...this.state.values, favorites: favorite, prevFav : prev}
+//       });
+
+//       console.log("After: " + this.state.values.favorites);
+
+//   }
+
+
+//   handleRedo = () => {
+
+//     // if(historyStep === history.length-1) {
+//     //   return;
+//     // }
+//     historyStep += 1;
+//     // const next = history[historyStep-2];
+//     const next = "";
+//     this.setState({
+//        values:{...this.state.values,favorites:next}
+//     });
+//   }
+  
+//   render() {
+
+//     return (
+//         <React.Fragment>
+
+//           <button onClick = {this.handleUndo}>undo</button>
+//           <button onClick = {this.handleRedo}>redo</button>
+//           <h1>Posts</h1>
+
+//           <select  onChange={this.handleChange}>
+//             <option value="hot">hot</option>
+//             <option value="desc">descending</option>
+//             <option value="asce">ascending</option>
+//           </select>
+
+//           <ol>
+//           {this.state.values.posts.map((post) => 
+//             <li> 
+//               <a href={post.data.url} style={{cursor: 'pointer'}}>{post.data.title}</a>
+//                 {post.data.ups}
+//                 <span style={{cursor: 'pointer'}} onClick={()=>this.addhandleClick(post)} >★</span>
+//             </li>)}
+//           </ol>
+
+//           <h3>Favorites</h3>
+
+//           <ol>
+//           {this.state.values.favorites.map((favorite) => 
+//             <li> 
+//               <a href={favorite.data.url} style={{cursor: 'pointer'}}>{favorite.data.title}</a>
+//                 {favorite.data.ups}
+//                 <span style={{cursor: 'pointer'}}onClick={()=>this.removehandleClick(favorite)} >X</span>
+//             </li>)}
+//           </ol>
+          
+//         </React.Fragment>
+//     );
+//   }
+
+// }
+
+// export default Posts;
 
